8c9d3f8d5a4ec898b7adcb8bd2a2dae25dd8ca08
juliachupak <prodves@gmail.com>
2024-08-07 20:00:57 +0300

    homework 1

---
+++ docs/home-work-1.js
@@ -0,0 +1,44 @@
+console.warn('Homework 1');
+
+// Task 1
+const helloString = 'hello';
+const owuString = 'owu';
+const comString = 'com';
+const uaString = 'ua';
+console.log(`${helloString}, ${owuString}, ${comString}, ${uaString}`);
+
+const num1 = 1;
+const num2 = 10;
+const num3 = -999;
+const num4 = 123;
+const num5 = 3.14;
+const num6 = 2.7;
+const num7 = 16;
+console.log(num1, num2, num3, num4, num5, num6, num7);
+
+const bool1 = true;
+const bool2 = false;
+console.log(bool1, bool2);
+
+
+
+// Task 2
+let firstName, middleName, lastName;
+firstName = 'John';
+middleName = 'Junior';
+lastName = 'Doe';
+const person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+
+
+// Task 3
+let a = 100;
+let b = '100';
+let c = true;
+console.log('type for a ->', typeof a);
+console.log('type for b ->', typeof b);
+console.log('type for c ->', typeof c);
+
+
+
